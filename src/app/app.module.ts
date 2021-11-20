69292af0a46edebae505f8748f61f23b4ea29cea
ersimont <8042088+ersimont@users.noreply.github.com>
2021-11-19 22:46:09 -0500

    manually apply CLI diff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,9 @@ import { environment } from '../environments/environment';
     MatListModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
     }),
   ],
   bootstrap: [AppComponent],

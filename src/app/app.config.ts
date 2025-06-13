b5636f02a13b8855cd5ff8a42e5a2420bb3207e3
ersimont <8042088+ersimont@users.noreply.github.com>
2025-06-13 07:52:01 -0400

    manually apply CLI diffs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,17 @@
 import {
   ApplicationConfig,
   isDevMode,
+  provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideServiceWorker } from '@angular/service-worker';
 import { provideMatIcons } from '@s-libs/ng-mat-core';
 import { icons } from './icons';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideAnimationsAsync(),
     provideMatIcons(icons),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
